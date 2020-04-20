6a6d44fbd02e88840fc334300a70a7e8bb9300ef
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-04-20 23:00:26 +0900

    DDL) 일별 시간축 테이블 생성 스크립트  : yyyyMMdd 시간축 테이블 이름 변경

--- sql/ddl/axis/date_series_dd.sql
+++ sql/ddl/axis/date_series_dd.sql
@@ -1,15 +1,15 @@
 /**
  * yyyyMMdd 시간축 테이블
  */
-create table DATE_SERIES_DD
+create table DATE_AXIS_DD
 (
     v_yyyy varchar(4) not null,
     v_mm   varchar(2) not null,
     v_dd   varchar(2) not null,
     v_date varchar(6) not null
         primary key
-);
+)
+    comment '일별 시간축';
 
 create index DATE_SERIES_DD_v_date_index
-    on DATE_SERIES_DD (v_date);
-
+    on DATE_AXIS_DD (v_date);
