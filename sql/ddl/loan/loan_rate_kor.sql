88eaeafecd2870d7687f8f2e1ca314eb0e18ea80
kyle.sgjung <kyle.sgjung@kakaocorp.com>
2020-04-21 23:29:29 +0900

    DDL 추가

---
+++ sql/ddl/loan/loan_rate_kor.sql
@@ -0,0 +1,15 @@
+create table loan_rate_kor
+(
+    STAT_NAME  text   null,
+    STAT_CODE  text   null,
+    ITEM_CODE1 text   null,
+    ITEM_NAME1 text   null,
+    ITEM_CODE2 text   null,
+    ITEM_NAME2 text   null,
+    ITEM_CODE3 text   null,
+    ITEM_NAME3 text   null,
+    DATA_VALUE double null,
+    TIME       text   null
+)
+    comment '한국 중앙은행 정책금리';
+
---
+++ sql/ddl/loan/loan_rate_usa.sql
@@ -0,0 +1,14 @@
+create table loan_rate_usa
+(
+    STAT_NAME  text   null,
+    STAT_CODE  text   null,
+    ITEM_CODE1 text   null,
+    ITEM_NAME1 text   null,
+    ITEM_CODE2 text   null,
+    ITEM_NAME2 text   null,
+    ITEM_CODE3 text   null,
+    ITEM_NAME3 text   null,
+    DATA_VALUE double null,
+    TIME       text   null
+);
+
